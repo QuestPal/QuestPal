be1a3996cff1991fd411a9adadfd2e32992c5bb4
Phillip Troutman <philliptroutman21@gmail.com>
2018-05-16 14:58:33 -0700

    changed index.html connected to css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 import Store from './store/configureStore';
 import App from './components/App.jsx';
+import CSS from '../public/styles.css';
 
 const store = Store();
 
